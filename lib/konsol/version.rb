fa752cde74e80f04add30eb3d8cf2b263e1a9e32
Andrew Kodkod <678665+akodkod@users.noreply.github.com>
2025-12-21 15:29:08 +0000

    Initialize :octocat:

---
+++ lib/konsol/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Konsol
+  VERSION = "0.1.0"
+end
---
+++ spec/konsol_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Konsol do
+  it "has a version number" do
+    expect(Konsol::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
